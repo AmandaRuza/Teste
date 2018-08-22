16590c682a47a05ab7ea4201b17644e8592d7996
081160045 <081160045@HPFS-CE-LAB6916.cefsa.corp.local>
2018-08-21 22:33:44 -0300

    Teste

---
+++ Idade/src/Idade.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Idade {
+
+	public static void main (String[] args) {
+		//TODO Auto-generated method stub
+		
+		//Dica: sout + ctrl + " "
+		System.out.println("Idade...");
+		
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Informe o ano em que vc conheceu o amor da sua vida: ");
+		
+		try {
+			System.out.println("Você conheceu o amor da sua vida há: " + (2018 - scanner.nextInt()) + " anos!");
+		}
+		catch (Exception e) {
+			System.out.println("Ops! Em que momento tudo deu errado, anão?");
+			//e.printStackTrace();
+			//System.out.println(e.getLocalizedMessage());
+		}
+		
+		scanner.close();
+	}
+}
